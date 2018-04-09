17512121006e8e0e133df3ddc417c94654b86b8d
Daniel Lucas <daniel.icapui@hotmail.com>
2018-04-09 09:16:57 -0300

    Adicionando arquivo com funções geométricas

---
+++ geo.py
@@ -0,0 +1,17 @@
+def area_circulo(r):
+    return 3.14*(r**2)
+
+def area_quadrada(l):
+    return l*l
+
+def area_triangulo(b,a):
+    return b*a
+
+def area_retangulo(b,a):
+    return b*a
+
+def  area_losangulo(d1,d2):
+    return (d1*d2)/2
+
+def area_pentagono(a,p):
+    return (a*p)/2
